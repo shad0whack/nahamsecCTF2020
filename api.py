72ac2564b2995ab46b7f280d64c570a130884432
Yasser Ali <y@sserali.com>
2020-01-11 11:42:05 +0400

    Create api.py

---
+++ api.py
@@ -0,0 +1,6 @@
+app = Flask(__name__)
+api = Api(app, version='0.1', title='Get_Flag API',default='nahamsecCTF2020',default_label='updates to ve pulled soon!',
+    description='Oh boy, everyone is talking about WW3 and you still hacking? even our developer was in rush that he left the credentials somewhere!', doc='/swagger'
+)
+# this is damn old repo. Crdentials used are username: BugHunters
+password: 4dawin
